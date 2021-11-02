4449894e4259f41458da780fcd99c63d6836d2d6
Young-Jun Lee <passing2961@gmail.com>
2021-11-02 14:10:56 +0900

    update script

--- Homework3/run.sh
+++ Homework3/run.sh
@@ -2,6 +2,8 @@
 
 if [ "$1" = "train" ]; then
     python3 run.py train --train-src=./data/train.de-en.de.wmixerprep --train-tgt=./data/train.de-en.en.wmixerprep --dev-src=./data/valid.de-en.de --dev-tgt=./data/valid.de-en.en --vocab=./data/vocab.json --cuda
+elif [ "$1" = "test" ]; then
+    python3 run.py decode sample_model.bin ./data/test.de-en.de ./data/test.de-en.en outputs/sample_outputs.txt
 elif [ "$1" = "vocab" ]; then
 	python3 vocab.py --train-src=./data/train.de-en.de.wmixerprep --train-tgt=./data/train.de-en.en.wmixerprep ./data/vocab.json
 else
